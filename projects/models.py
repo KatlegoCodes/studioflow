8c233879bb3b5a5b47b46a59e97b8719d9a6bbcb
Frost <moosemashegona@gmail.com>
2026-01-19 12:06:06 +0200

    Add core domain models for clients, projects and billing

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.models import User
 from django.db import models
+from clients.models import Client
 
-# Create your models here.
+class Project(models.Model):
+    STATUS_ChOICES = (
+        ('pending', 'Pending'),
+        ('active', 'Active'),
+        ('completed', 'Completed'),
+        ('on_hold', 'On Hold'),
+    )
+
+    name = models.CharField(max_length=255)
+    client = models.ForeignKey(
+        Client,
+        on_delete=models.CASCADE,
+    )
+
+    description = models.TextField()
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_ChOICES,
+        deafault='pending'
+    )
+
+    start_date = models.DateField()
+    due_date = models.DateField()
+
+    assign_staff = models.ManyToManyField(
+        User,
+        blank=True
+    )
+
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
