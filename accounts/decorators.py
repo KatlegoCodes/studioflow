eca0a9ed2295abf0b14a36d9ad8ceb652d9906ae
Frost <moosemashegona@gmail.com>
2026-01-20 16:50:49 +0200

    added decorators to protect admin previliges

---
+++ accounts/decorators.py
@@ -0,0 +1,11 @@
+
+from django.contrib.auth.models import user_passes_test
+from .utils import is_admin, is_staff
+
+def admin_required(view_func):
+    return user_passes_test(is_admin)(view_func)
+
+def staff_required(view_func):
+    return user_passes_test(
+        lambda u: is_admin(u) or is_staff(u)
+    )(view_func)
