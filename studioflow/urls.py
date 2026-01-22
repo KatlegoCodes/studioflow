aa01b7bb9a09bf25e255b5c06c670dde49054bb6
Frost <moosemashegona@gmail.com>
2026-01-22 11:14:01 +0200

    Added login functionality for both user and admin

--- studioflow/urls.py
+++ studioflow/urls.py
@@ -21,6 +21,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path("", RedirectView.as_view(pattern_name="client_list", permanent=False)),
     path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
     path("clients/", include("clients.urls")),
 ]
 
