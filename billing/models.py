76b55ad81c572d52f6a982872e234933326cda04
Frost <moosemashegona@gmail.com>
2026-01-26 17:23:52 +0200

    Update Invoice model with optional project with optional field and rename date_by to created

--- billing/models.py
+++ billing/models.py
@@ -13,11 +13,16 @@ class Invoice(models.Model):
     client = models.ForeignKey(
         Client,
         on_delete=models.CASCADE,
+        related_name='invoices'
     )
 
     project = models.ForeignKey(
         Project,
-        on_delete=models.CASCADE
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='invoices'
+
     )
 
     amount = models.DecimalField(
